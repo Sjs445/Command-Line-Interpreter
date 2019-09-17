405d99cd0ec5469651181c82f5d729ee061df4d5
sjs445 <sjs445@csu.fullerton.edu>
2019-09-16 21:01:53 -0700

    progress

--- myShell.cpp
+++ myShell.cpp
@@ -1,9 +1,28 @@
+// myShell.cpp : Simple Command Line Interpreter.
+// Copyright (c) Shane Spangenberg 9/16/2019
+
 #include <iostream>
+#include <string>
+#define MAX 100
 
 using namespace std;
 
+
 int main()
 {
+    cout<<"Welcome to my command line interpreter...\n";
+    cout<<"Enter a command to begin.\n";
+    cout<<"--------------------------------------------\n";
 
+    char input[MAX];
+
+
+   // while(1)
+   // {
+        fgets(input, MAX, stdin);
+ 
+   // }
+
+        
     return 0;
 }
